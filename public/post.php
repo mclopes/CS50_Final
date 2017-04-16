5e32ad6e617ac8db79998fe38f09f65702efeda1
Marcelo Lopes <mlopes@miami.edu>
2017-04-15 20:11:01 -0400

    post upload control

--- public/post.php
+++ public/post.php
@@ -3,26 +3,61 @@
     // configuration
     require("../includes/config.php"); 
 
-    if ($_FILES["image"]["error"] > 0)
-    {
-        apologize("No image selected");
-    }
-    else
-    {
-        move_uploaded_file($_FILES["image"]["tmp_name"],"../public/images/" . $_FILES["image"]["name"]);
-        //echo"<font size = '5'><font color=\"#0CF44A\">SAVED<br>";
-
-        $file="../public/images/".$_FILES["image"]["name"];
-        $sql="INSERT INTO posts (user_id, post_name, post_city, post_phone, post_email, picture, animal_type, animal_health) 
-                VALUES ('$_SESSION["id"]', 'post1','city1','123','pa@gmail.com', '$file', 'dog', 'good')";
-
-        if (!mysql_query($sql))
+        if((empty($_POST["post_name"]) || 
+            empty($_POST["post_city"]) || 
+            empty($_POST["post_phone"])))
+         {
+            apologize("Please fill out all info");
+         }
+        else
         {
-            die('Error: ' . mysql_error());
-        }
-        echo "<font size = '5'><font color=\"#0CF44A\">SAVED TO DATABASE";
+            // create a variable
+            $post_name=$_POST['post_name'];
+            $post_city=$_POST['post_city'];
+            $post_phone=$_POST['post_phone'];
+            $post_email=$_POST['post_email'];
+            //IMAGE
+            $animal_type=$_POST['type'];
+            $animal_sex=$_POST['sex'];
+            $animal_name=$_POST['animal_name'];
+            $animal_health=$_POST['health'];
+            $animal_age=$_POST['age'];
 
-    }
+
+        //Execute the query
+ 
+             mysqli_query($conn,"INSERT INTO posts (post_name, post_city, post_phone, post_email, animal_type, animal_sex, animal_name, animal_health, animal_age)
+				                    VALUES('$post_name','$post_city','$post_phone','$post_email','$animal_type','$animal_sex','$animal_name','$animal_health','$animal_age')");
+
+        }
+
+
+        
+
+
+
+
+
+    // if ($_FILES["image"]["error"] > 0)
+    // {
+    //     apologize("No image selected");
+    // }
+    // else
+    // {
+    //     move_uploaded_file($_FILES["image"]["tmp_name"],"../public/images/" . $_FILES["image"]["name"]);
+    //     //echo"<font size = '5'><font color=\"#0CF44A\">SAVED<br>";
+
+    //     $file="../public/images/".$_FILES["image"]["name"];
+    //     $sql="INSERT INTO posts (user_id, post_name, post_city, post_phone, post_email, picture, animal_type, animal_health) 
+    //            VALUES ('$_SESSION["id"]', 'post1','city1','123','pa@gmail.com', '$file', 'dog', 'good')";
+
+    //     if (!mysql_query($sql))
+    //     {
+    //         die('Error: ' . mysql_error());
+    //     }
+    //     echo "<font size = '5'><font color=\"#0CF44A\">SAVED TO DATABASE";
+
+    //}
 
    //mysql_close();
 
--- views/post_upload.php
+++ views/post_upload.php
@@ -9,7 +9,7 @@
             <input autocomplete="off" autofocus class="form-control" name="post_city" placeholder="city" type="text"/>
         </div>
         <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="post_phone" placeholder="name" type="number"/>
+            <input autocomplete="off" autofocus class="form-control" name="post_phone" placeholder="phone" type="text"/>
         </div>
         <div class="form-group">
             <input autocomplete="off" autofocus class="form-control" name="post_email" placeholder="email" type="text"/>
@@ -43,7 +43,7 @@
         <div class="form-group">
             <button class="btn btn-default" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                Log In
+                Submit
             </button>
         </div>
     </fieldset>
