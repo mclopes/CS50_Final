c1e38d84920c05aa2d052a1ed9844cadd5b6d603
Karel Bofill <scherzohradec@gmail.com>
2017-04-18 10:44:23 -0400

    Done with register!

--- public/register.php
+++ public/register.php
@@ -36,21 +36,19 @@
                 $is_shelter = 0;
             }
 
-            //Execute the query
+            //Execute the query for users table
  
             $result = mysqli_query($conn,"INSERT INTO users (user_name, password, email, is_shelter)
 				                    VALUES('$name','$password','$email','$is_shelter')");
 
             
-
-            //CS50::query("INSERT IGNORE INTO users (username, hash, cash) VALUES(?, ?, 10000.0000)", $_POST["username"], password_hash($_POST["password"], PASSWORD_DEFAULT));
             if($result === false)
             {
               apologize("Couldn't be saved to database");
             }
             else
             {
-              //$msg = "Thanks for signing up for our service!";
+              //setting the value for the current session
               $rows =  mysqli_query($conn,"SELECT LAST_INSERT_ID() AS id");
               if (mysqli_num_rows($rows) > 0)
               {
@@ -63,6 +61,7 @@
                   apologize(":(");
               }
 
+              //If user has a shelter
               if ($is_shelter === 1)
               {
                   $capacity = $_POST['capacity'];
@@ -74,10 +73,7 @@
                   $address_province = $_POST['address_province'];
                   $address_postal_code = $_POST['address_postal_code'];
 
-                  
-                //   apologize("avilab: ".$availability."type: ".$type."ad num: ".$address_number."street: ".$address_street."city: ".$address_city."province: ".$address_province."CP: ".$address_postal_code."user id: ".$id."capacity: ".$capacity);
-                  
-                  
+                  //Executing query for shelters table
                   $result2 = mysqli_query($conn,"INSERT INTO shelters (capacity, availability, type, user_id)
 				                    VALUES('$capacity','$availability','$type','$id')");
                 
@@ -87,29 +83,18 @@
                     }
                     else
                     {
+                        //getting value for the shelter id to link it to shelter address table
                         $rows_shelter =  mysqli_query($conn,"SELECT LAST_INSERT_ID() AS shelter_id");
                         if (mysqli_num_rows($rows_shelter) > 0)
                         {
                             $row_shelter = mysqli_fetch_assoc($rows_shelter);
                             $id_shelter = $row_shelter["shelter_id"];
-                        // $_SESSION["shelter_id"] = $id_shelter; 
                         }
 
-                        //Error checkin
-                        // if (!mysqli_query($conn,"INSERT INTO shelter_address (shelter_id, address_number,address_street, address_city, address_province, address_postal_code)
-                        //--                 VALUES('$id_shelter', $address_number','$address_street','$address_city','$address_province', '$address_postal_code')"))
-                        // {
-                        //     echo("Error description: " . mysqli_error($conn));
-                        // }
-
                         
-                        $result3 = mysqli_query($conn,"INSERT INTO shelter_address (shelter_id, address_number,address_street, address_city, address_province, address_postal_code)
-                                         VALUES('$id_shelter', $address_number','$address_street','$address_city','$address_province', '$address_postal_code')");   
-
-                    
-                       
-                       
-                        apologize("ad num: ".$address_number." street: ".$address_street." city: ".$address_city." province: ".$address_province." CP: ".$address_postal_code." id shelter: ".$id_shelter);
+                        //Executing the query for shelter_address table
+                        $result3 = mysqli_query($conn,"INSERT INTO shelter_address (shelter_id, address_number, address_street, address_city, address_province, address_postal_code)
+                                        VALUES('$id_shelter', '$address_number','$address_street','$address_city','$address_province', '$address_postal_code')");   
                         
                         if($result3 === false)
                         {
